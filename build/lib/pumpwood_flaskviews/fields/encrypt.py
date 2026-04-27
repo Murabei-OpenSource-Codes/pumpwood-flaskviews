42b8d0163d3473c99b6cd8c76c63dea2ee79eb9a
André Andrade Baceti <a.baceti@murabei.com>
2026-04-27 08:58:25 -0300

    Building a new version 1.5.17

--- build/lib/pumpwood_flaskviews/fields/encrypt.py
+++ build/lib/pumpwood_flaskviews/fields/encrypt.py
@@ -24,21 +24,30 @@ class EncryptedField(fields.Field):
         """Convert object to dictionary (JSON)."""
         return value
 
-    def _deserialize(self, value, attr, data):
+    def deserialize(self, value, attr, data):
         """Convert dictionary to object."""
+        print("value:", value)
+        print("attr:", attr)
+        print("data:", data)
         overwrited_data = _get_overwrite_audit(
             field=self, data=data, current_user=None,
             raise_not_superuser=False)
+        print("overwrited_data:", overwrited_data)
         if overwrited_data is not missing:
-            encrypted_value = encrypt_obj.encrypt(value=value)
+            encrypted_value = encrypt_obj.encrypt(value=overwrited_data)
             return encrypted_value
 
         # Access the existing object from the schema context
         existing_obj = getattr(self.parent, "instance", None)
         if existing_obj is not None:
             # If the object was already setted, get the value from the field
+            # this is used when updating an object, this will not make
+            # recursive encription when and object is updated with encripted
+            # field already encrypted.
             existing_value = getattr(existing_obj, attr, None)
             return existing_value
         else:
+            # If the object was not setted, encrypt the value on the first
+            # save
             encrypted_value = encrypt_obj.encrypt(value=value)
             return encrypted_value
